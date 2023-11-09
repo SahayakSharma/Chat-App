f366fa078db81c56cb0692ba8f15cf0fbe34852c
SahayakSharma <sahayaksharma6@gmail.com>
2023-11-09 16:01:52 +0530

    added login page

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -4,10 +4,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './LoginPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path:"/login",
+    element:<LoginPage/>
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
